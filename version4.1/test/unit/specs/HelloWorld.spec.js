51fa6a43d5b1e93168bfc6d696bbe84e241cbf8a
zhaoying98sjtu <zhaoying98@sjtu.edu.cn>
2017-12-15 18:58:32 +0800

    12.15课后补上传依赖，单元测试仍然有问题

--- version4.1/test/unit/specs/HelloWorld.spec.js
+++ version4.1/test/unit/specs/HelloWorld.spec.js
@@ -5,7 +5,7 @@ describe('HelloWorld.vue', () => {
   it('should render correct contents', () => {
     const Constructor = Vue.extend(HelloWorld)
     const vm = new Constructor().$mount()
-    expect(vm.$el.querySelector('.navbar navbar-default h2').textContent)
-    .to.equal('Information Website for SE-2016')
+    expect(vm.$el.querySelector('.hello h1').textContent)
+    .to.equal('Welcome to Your Vue.js App')
   })
 })
